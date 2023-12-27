d800894d7349738558870c5f91f58f1b889efeb1
FaustXL <143204667+FaustXL@users.noreply.github.com>
2023-12-27 16:37:57 +0800

    新增学生界面

---
+++ StudentSystem/src/main/java/org/example/UI/SMain.java
@@ -0,0 +1,41 @@
+package org.example.UI;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SMain {
+    private JPanel SMain;
+    private JTabbedPane tabbedPane1;
+    private JPanel Myself;
+    private JPanel Class;
+    private JPanel Homework;
+    private JPanel Setting;
+    private JLabel Sname;
+    private JLabel Ssex;
+    private JLabel Smajor;
+    private JLabel SclassAndGrade;
+    private JLabel Sschool;
+    private JTable ClassTable;
+    private JTable AllClassTable;
+    private JTable HomeWorkTable;
+    private JLabel theme;
+    private JComboBox themeComboBox;
+    private JButton LogOut;
+
+    public SMain(){
+        try {
+            UIManager.setLookAndFeel( new FlatIntelliJLaf());
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+        JFrame frame = new JFrame("SMain");
+        frame.setPreferredSize(new Dimension(1000,800));
+        frame.setContentPane(SMain);
+        SwingUtilities.updateComponentTreeUI(frame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
